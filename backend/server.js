ca33732b614e954b57029b630c5e8f608d82cf33
Nghia Hoang <nghiahoang880@gmail.com>
2025-06-25 22:34:57 +0700

    feat: connect mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,22 @@
 import app from "./app.js";
 import config from "./src/config/index.js";
+import MongoDB from "./src/utils/mongodb.util.js";
 
 const PORT = config.app.port;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+async function startServer() {
+    try {
+        await MongoDB.connect(config.db.uri);
+        console.log("Connected to the database!");
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+
+    } catch (error) {
+        console.log(`Cannot connect to the database! ${error}`);
+        process.exit();
+    }
+}
+
+startServer();
