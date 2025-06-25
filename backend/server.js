d92a278e56e8e8ca3cda8c20aa8e9bd5e6226350
Nghia Hoang <nghiahoang880@gmail.com>
2025-06-25 22:27:55 +0700

    chore: initial backend folder

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+import app from "./app.js";
+import config from "./src/config/index.js";
+
+const PORT = config.app.port;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
