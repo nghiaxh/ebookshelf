cb9356294ca569587fee6c715c65477adee00639
Nghia Hoang <nghiahoang880@gmail.com>
2025-06-26 12:28:25 +0700

    fix: misplaced code

---
+++ backend/src/models/book.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const bookSchema = mongoose.Schema({
+    title: {
+        type: String, required: true,
+    },
+    price: {
+        type: Number, required: true,
+    },
+    quantity: {
+        type: Number, required: true,
+    },
+    published_year: {
+        type: Number, required: true,
+    },
+    publisher_id: {
+        type: mongoose.Schema.Types.ObjectId, required: true,
+    },
+    author: {
+        type: String, required: true,
+    },
+    description: {
+        type: String, required: true,
+    },
+    image: {
+        type: String, default: ""
+    }
+});
+
+const book = mongoose.model("Book", bookSchema);
+export default book;
