0af9186468b355acd03d24d5ebed9635b1bd68b0
Nghia Hoang <nghiahoang880@gmail.com>
2025-06-30 22:20:05 +0700

    feat: create auth middleware

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,24 @@
+import ApiError from "../api-error.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default function auth(req, res, next) {
+    const token = req.headers["authorization"]?.split(" ")[1];
+
+    if (!token) {
+        return next(new ApiError(401, "Invalid token"));
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        if (error.name === "TokenExpiredError") {
+            return next(new ApiError(401, "Token has expired"));
+        }
+        return next(new ApiError(401, "Invalid token"));
+    }
+};
