b8d05b42d10f773c00b96761b15b632d15fa79d8
Nghia Hoang <nghiahoang880@gmail.com>
2025-07-01 20:35:07 +0700

    chore: update config

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -3,7 +3,7 @@ const config = {
         port: process.env.PORT || 3000,
     },
     db: {
-        uri: process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/ebookshelf"
+        uri: process.env.MONGODB_URL || "mongodb://localhost:27017/ebookshelf"
     },
 };
 
