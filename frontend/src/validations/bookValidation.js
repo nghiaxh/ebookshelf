963f844d2cf560eb969ba2eaac71cee3e9de17d7
Nghia Hoang <nghiahoang880@gmail.com>
2025-07-21 01:07:34 +0700

    feat: add new validation for book schema

--- frontend/src/validations/bookValidation.js
+++ frontend/src/validations/bookValidation.js
@@ -3,6 +3,8 @@ import * as yup from "yup";
 export const bookSchema = yup.object({
     book_title: yup.string().required("Vui lòng nhập tựa sách"),
     author: yup.string().required("Vui lòng nhập tác giả"),
-    quantity: yup.number().required("Vui lòng nhập số lượng").min(1, "Số lượng sách tối thiểu là 1 quyển"),
-    publisher_id: yup.string().required("Vui lòng chọn nhà xuất bản")
+    quantity: yup.number().required("Vui lòng nhập số lượng").min(1, "Số lượng sách tối thiểu là 1 quyển").typeError("Vui lòng nhập số lượng"),
+    publisher_id: yup.string().required("Vui lòng chọn nhà xuất bản"),
+    price: yup.number().required("Vui lòng nhập đơn giá").min(0, "Đơn giá sách tối thiểu là 0đ").typeError("Vui lòng nhập đơn giá"),
+    published_year: yup.number().required("Vui lòng nhập năm xuất bản").min(0, "Năm xuất bản không hợp lệ").typeError("Vui lòng nhập năm xuất bản")
 });
